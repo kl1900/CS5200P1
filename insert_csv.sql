3be151670f994aebf48d7c8d0e797402c8752b0e
Cy663 <tx090817@gmail.com>
2025-03-18 14:58:57 -0700

    added csvs and edited insert_csv.sql

--- insert_csv.sql
+++ insert_csv.sql
@@ -1,8 +1,99 @@
 USE Practicum1;
 
-LOAD DATA INFILE '/var/lib/mysql-files/data/PlayerRank.csv' 
-INTO TABLE PlayerRank 
+LOAD DATA INFILE '/var/lib/mysql-files/data/Player.csv'
+INTO TABLE Player
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
-IGNORE 1 ROWS
+IGNORE 1 ROWS;
+
+LOAD DATA INFILE '/var/lib/mysql-files/data/PlayerRank.csv'
+INTO TABLE PlayerRank
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+LOAD DATA INFILE '/var/lib/mysql-files/data/RankDecider.csv'
+INTO TABLE RankDecider
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+LOAD DATA INFILE '/var/lib/mysql-files/data/BanStatus.csv'
+INTO TABLE BanStatus
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+LOAD DATA INFILE '/var/lib/mysql-files/data/BanDecider.csv'
+INTO TABLE BanDecider
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+LOAD DATA INFILE '/var/lib/mysql-files/data/PaidPlayer.csv'
+INTO TABLE PaidPlayer
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+LOAD DATA INFILE '/var/lib/mysql-files/data/Session.csv'
+INTO TABLE Session
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+LOAD DATA INFILE '/var/lib/mysql-files/data/Result.csv'
+INTO TABLE Result
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+LOAD DATA INFILE '/var/lib/mysql-files/data/Game.csv'
+INTO TABLE Game
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+LOAD DATA INFILE '/var/lib/mysql-files/data/PlayerGameSession.csv'
+INTO TABLE PlayerGameSession
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+LOAD DATA INFILE '/var/lib/mysql-files/data/GameComment.csv'
+INTO TABLE GameComment
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+LOAD DATA INFILE '/var/lib/mysql-files/data/Action.csv'
+INTO TABLE Action
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+LOAD DATA INFILE '/var/lib/mysql-files/data/Achievements.csv'
+INTO TABLE Achievements
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+LOAD DATA INFILE '/var/lib/mysql-files/data/PlayerAchievement.csv'
+INTO TABLE PlayerAchievement
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
